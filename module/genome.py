4f3b1c36fdd08c2227f8e0a7879d95b9de2157d8
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-06-10 22:14:50 +0900

    initial commit

---
+++ module/genome.py
@@ -0,0 +1,180 @@
+import os
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from module.simulator import Simulator
+simulator = Simulator()
+submission_ini =\
+    pd.read_csv(os.path.join(Path(__file__).resolve().parent,
+                'sample_submission.csv'))
+order_ini =\
+    pd.read_csv(os.path.join(Path(__file__).resolve().parent, 'order.csv'))
+
+
+class Genome():
+    def __init__(self, score_ini, input_len, output_len_1, output_len_2,
+                 h1=50, h2=50, h3=50):
+        # initializing score
+        self.score = score_ini
+
+        # number of nodes in hidden layers
+        self.hidden_layer1 = h1
+        self.hidden_layer2 = h2
+        self.hidden_layer3 = h3
+
+        # Generating weights for Event NN
+        self.w1 = np.random.randn(input_len, self.hidden_layer1)
+        self.w2 = np.random.randn(self.hidden_layer1, self.hidden_layer2)
+        self.w3 = np.random.randn(self.hidden_layer2, self.hidden_layer3)
+        self.w4 = np.random.randn(self.hidden_layer3, output_len_1)
+
+        # Generating weights for MOL stock NN
+        self.w5 = np.random.randn(input_len, self.hidden_layer1)
+        self.w6 = np.random.randn(self.hidden_layer1, self.hidden_layer2)
+        self.w7 = np.random.randn(self.hidden_layer2, self.hidden_layer3)
+        self.w8 = np.random.randn(self.hidden_layer3, output_len_2)
+
+        # Event categories
+        self.mask = np.zeros([5], np.bool)  # Checks available events
+        self.event_map = {0: 'CHECK_1', 1: 'CHECK_2', 2: 'CHECK_3',
+                          3: 'CHECK_4', 4: 'PROCESS'}
+
+        # Status parameters
+        self.check_time = 28    # CHECK -1/hr, 28 if process_time >=98
+        self.process_ready = False  # False if CHECK is required else True
+        self.process_mode = 0   # Represents item in PROCESS; 0 represents STOP
+        self.process_time = 0   # PROCESS +1/hr, CHANGE +1/hr, 140 at max
+
+    def update_mask(self):
+        self.mask[:] = False
+        if self.process_ready is False:
+            if self.check_time == 28:
+                self.mask[:4] = True  # ambiguity : corresponds to event_map
+            if self.check_time < 28:
+                self.mask[self.process_mode] = True  # ambiguity : 0 and STOP
+        if self.process_ready is True:
+            self.mask[4] = True  # ambiguity : corresponds to event_map
+            if self.process_time > 98:
+                self.mask[:4] = True
+
+    def forward(self, inputs):
+        # Event NN
+        net = np.matmul(inputs, self.w1)
+        net = self.linear(net)
+        net = np.matmul(net, self.w2)
+        net = self.linear(net)
+        net = np.matmul(net, self.w3)
+        net = self.sigmoid(net)
+        net = np.matmul(net, self.w4)
+        net = self.softmax(net)
+        net += 1
+        net = net * self.mask
+        out1 = self.event_map[np.argmax(net)]
+
+        # MOL stock NN
+        net = np.matmul(inputs, self.w5)
+        net = self.linear(net)
+        net = np.matmul(net, self.w6)
+        net = self.linear(net)
+        net = np.matmul(net, self.w7)
+        net = self.sigmoid(net)
+        net = np.matmul(net, self.w8)
+        net = self.softmax(net)
+        out2 = np.argmax(net)
+        out2 /= 2
+        return out1, out2
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def softmax(self, x):
+        return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+    def linear(self, x):
+        return x
+
+    def create_order(self, order):
+        for i in range(30):
+            order.loc[91+i, :] = ['0000-00-00', 0, 0, 0, 0]
+        return order
+
+    def predict(self, order):
+        order = self.create_order(order)
+        self.submission = submission_ini
+        self.submission.loc[:, 'PRT_1':'PRT_4'] = 0
+        for s in range(self.submission.shape[0]):
+            self.update_mask()
+            inputs = np.array(order.loc[s//24:(s//24+30), 'BLK_1':'BLK_4']).\
+                reshape(-1)
+            inputs = np.append(inputs, s % 24)
+            out1, out2 = self.forward(inputs)
+
+            if out1 == 'CHECK_1':
+                if self.process_ready is True:
+                    self.process_ready = False
+                    self.check_time = 28
+                self.check_time -= 1
+                self.process_mode = 0
+                if self.check_time == 0:
+                    self.process_ready = True
+                    self.process_time = 0
+            elif out1 == 'CHECK_2':
+                if self.process_ready is True:
+                    self.process_ready = False
+                    self.check_time = 28
+                self.check_time -= 1
+                self.process_mode = 1
+                if self.check_time == 0:
+                    self.process_ready = True
+                    self.process_time = 0
+            elif out1 == 'CHECK_3':
+                if self.process_ready is True:
+                    self.process_ready = False
+                    self.check_time = 28
+                self.check_time -= 1
+                self.process_mode = 2
+                if self.check_time == 0:
+                    self.process_ready = True
+                    self.process_time = 0
+            elif out1 == 'CHECK_4':
+                if self.process_ready is True:
+                    self.process_ready = False
+                    self.check_time = 28
+                self.check_time -= 1
+                self.process_mode = 3
+                if self.check_time == 0:
+                    self.process_ready = True
+                    self.process_time = 0
+            elif out1 == 'PROCESS':
+                self.process_time += 1
+                if self.process_time == 140:
+                    self.process_ready = False
+                    self.check_time = 28
+
+            self.submission.loc[s, 'Event_A'] = out1
+            if self.submission.loc[s, 'Event_A'] == 'PROCESS':
+                self.submission.loc[s, 'MOL_A'] = out2
+            else:
+                self.submission.loc[s, 'MOL_A'] = 0
+
+        # MOL_A = 0 for the first 23 days
+        self.submission.loc[:24*23, 'MOL_A'] = 0
+
+        # Line A = Line B
+        self.submission.loc[:, 'Event_B'] = self.submission.loc[:, 'Event_A']
+        self.submission.loc[:, 'MOL_B'] = self.submission.loc[:, 'MOL_A']
+
+        # Initializing status parameters
+        self.check_time = 28
+        self.process_ready = False
+        self.process_mode = 0
+        self.process_time = 0
+
+        return self.submission
+
+
+def genome_score(genome):
+    submission = genome.predict(order_ini)
+    genome.submission = submission
+    genome.score, _ = simulator.get_score(submission)
+    return genome
---
+++ module/simulator.py
@@ -0,0 +1,218 @@
+import os
+import pandas as pd
+import numpy as np
+from pandas.api.types import is_numeric_dtype
+from pathlib import Path
+
+
+class Simulator:
+    def __init__(self):
+        self.sample_submission =\
+            pd.read_csv(os.path.join(Path(__file__).resolve().parent,
+                        'sample_submission.csv'))
+        self.max_count =\
+            pd.read_csv(os.path.join(Path(__file__).resolve().parent,
+                        'max_count.csv'))
+        self.stock =\
+            pd.read_csv(os.path.join(Path(__file__).resolve().parent,
+                        'stock.csv'))
+        order =\
+            pd.read_csv(os.path.join(Path(__file__).resolve().parent,
+                        'order.csv'), index_col=0)
+        order.index = pd.to_datetime(order.index)
+        self.order = order
+
+    def get_state(self, data):
+        if 'CHECK' in data:
+            return int(data[-1])
+        elif 'CHANGE' in data:
+            return int(data[-1])
+        else:
+            return np.nan
+
+    def cal_schedule_part_1(self, df):
+        columns = ['PRT_1', 'PRT_2', 'PRT_3', 'PRT_4']
+        df_set = df[columns]
+        df_out = df_set * 0  # ambiguity : why make them all zero?
+        # to make use of index column -> better to save it as template
+
+        p = 0.985  # Yield rate of PRTs
+        dt = pd.Timedelta(days=23)  # PRT processing time
+        end_time = df_out.index[-1]
+
+        for time in df_out.index:
+            out_time = time + dt
+            if end_time < out_time:
+                break
+            else:
+                for column in columns:
+                    set_num = df_set.loc[time, column]
+                    if set_num > 0:
+                        out_num =\
+                            np.sum(np.random.choice(2, set_num, p=[1-p, p]))
+                        df_out.loc[out_time, column] = out_num
+
+        df_out['MOL_1'] = 0.0
+        df_out['MOL_2'] = 0.0
+        df_out['MOL_3'] = 0.0
+        df_out['MOL_4'] = 0.0
+        df_out['BLK_1'] = 0.0
+        df_out['BLK_2'] = 0.0
+        df_out['BLK_3'] = 0.0
+        df_out['BLK_4'] = 0.0
+        return df_out
+
+    def cal_schedule_part_2(self, df, line='A'):
+        if line == 'A':
+            columns = ['Event_A', 'MOL_A']
+        elif line == 'B':
+            columns = ['Event_B', 'MOL_B']
+        else:
+            columns = ['Event_A', 'MOL_A']
+
+        schedule = df[columns].copy()
+
+        # state represents item number being processed
+        schedule['state'] = 0
+        schedule['state'] =\
+            schedule[columns[0]].apply(lambda x: self.get_state(x))
+        schedule['state'] = schedule['state'].fillna(method='ffill')
+        schedule['state'] = schedule['state'].fillna(0)
+
+        schedule_process = schedule.loc[schedule[columns[0]] == 'PROCESS']
+        df_out = schedule.drop(schedule.columns, axis=1)
+        df_out['PRT_1'] = 0.0
+        df_out['PRT_2'] = 0.0
+        df_out['PRT_3'] = 0.0
+        df_out['PRT_4'] = 0.0
+        df_out['MOL_1'] = 0.0
+        df_out['MOL_2'] = 0.0
+        df_out['MOL_3'] = 0.0
+        df_out['MOL_4'] = 0.0
+
+        p = 0.975  # Yield rate of MOL
+        times = schedule_process.index
+        for i, time in enumerate(times):
+            value = schedule.loc[time, columns[1]]  # MOL input
+            state = int(schedule.loc[time, 'state'])  # MOL input type
+            df_out.loc[time, 'PRT_'+str(state)] = -value  # reduce PRT
+            if i+48 < len(times):  # 48 hours = MOL process time
+                out_time = times[i+48]
+                # MOL output 48 hours later
+                df_out.loc[out_time, 'MOL_'+str(state)] = value*p
+
+        df_out['BLK_1'] = 0.0
+        df_out['BLK_2'] = 0.0
+        df_out['BLK_3'] = 0.0
+        df_out['BLK_4'] = 0.0
+        return df_out
+
+    def cal_stock(self, df, df_order):
+        df_stock = df * 0
+
+        blk2mol = {}
+        blk2mol['BLK_1'] = 'MOL_1'
+        blk2mol['BLK_2'] = 'MOL_2'
+        blk2mol['BLK_3'] = 'MOL_3'
+        blk2mol['BLK_4'] = 'MOL_4'
+
+        # daily production rate
+        cut = {}
+        cut['BLK_1'] = 506
+        cut['BLK_2'] = 506
+        cut['BLK_3'] = 400
+        cut['BLK_4'] = 400
+
+        # cut yield
+        p = {}
+        p['BLK_1'] = 0.851
+        p['BLK_2'] = 0.901
+        blk_diffs = []
+        for i, time in enumerate(df.index):
+            month = time.month
+            if month == 4:
+                p['BLK_3'] = 0.710
+                p['BLK_4'] = 0.700
+            elif month == 5:
+                p['BLK_3'] = 0.742
+                p['BLK_4'] = 0.732
+            elif month == 6:
+                p['BLK_3'] = 0.759
+                p['BLK_4'] = 0.749
+            else:
+                p['BLK_3'] = 0.0
+                p['BLK_4'] = 0.0
+
+            if i == 0:
+                df_stock.iloc[i] = df.iloc[i]
+            else:
+                # accumulate stocks
+                df_stock.iloc[i] = df_stock.iloc[i-1] + df.iloc[i]
+                for column in df_order.columns:  # column : BLK 1~4
+                    val = df_order.loc[time, column]  # orders by hours
+                    if val > 0:  # if there is an order
+                        mol_col = blk2mol[column]
+                        # MOL produced at time
+                        mol_num = df_stock.loc[time, mol_col]
+                        df_stock.loc[time, mol_col] = 0  # needless line
+
+                        # blk generated at time
+                        blk_gen = int(mol_num*p[column]*cut[column])
+                        blk_stock = df_stock.loc[time, column] + blk_gen
+                        # stock - order
+                        blk_diff = blk_stock - val
+
+                        df_stock.loc[time, column] = blk_diff
+                        blk_diffs.append(blk_diff)
+        return df_stock, blk_diffs
+
+    def subprocess(self, df):
+        out = df.copy()
+        column = 'time'
+
+        out.index = pd.to_datetime(out[column])
+        out = out.drop([column], axis=1)
+        out.index.name = column
+        return out
+
+    def add_stock(self, df, df_stock):
+        df_out = df.copy()
+        for column in df_out.columns:
+            df_out.iloc[0][column] =\
+                df_out.iloc[0][column] + df_stock.iloc[0][column]
+        return df_out
+
+    def order_rescale(self, df, df_order):
+        df_rescale = df.drop(df.columns, axis=1)
+        dt = pd.Timedelta(hours=18)  # represents 6 pm (deployment time)
+        for column in ['BLK_1', 'BLK_2', 'BLK_3', 'BLK_4']:
+            for time in df_order.index:
+                df_rescale.loc[time+dt, column] = df_order.loc[time, column]
+        df_rescale = df_rescale.fillna(0)
+        return df_rescale
+
+    def cal_score(self, blk_diffs):
+        """
+        Parameters :
+            blk_diffs : list of int
+        """
+        # Block Order Difference
+        blk_diff_m = 0
+        for item in blk_diffs:
+            if item < 0:
+                blk_diff_m = blk_diff_m + abs(item)
+        score = blk_diff_m
+        return score
+
+    def get_score(self, df):
+        df = self.subprocess(df)
+        out_1 = self.cal_schedule_part_1(df)
+        out_2 = self.cal_schedule_part_2(df, line='A')
+        out_3 = self.cal_schedule_part_2(df, line='B')
+        out = out_1 + out_2 + out_3
+        out = self.add_stock(out, self.stock)
+        # order now considers deployment
+        order = self.order_rescale(out, self.order)
+        out, blk_diffs = self.cal_stock(out, order)  # out = cumulative stocks
+        score = self.cal_score(blk_diffs)
+        return score, out
