e6c0c5ac5cba4860f468286a37b14d7668577fd1
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-06-28 11:23:03 +0900

    draft complete

--- module/simulator_new.py
+++ module/simulator_new.py
@@ -28,19 +28,24 @@ class Simulator:
         cut_yield.rename(columns={'date':'month'}, inplace=True)
         cut_yield['month'] = cut_yield['month'].astype(str).str.replace("2020", "2020-")
         cut_yield.set_index('month', inplace=True)
+        cut_yield = cut_yield / 100
         
         self.BLK = {'types' : ['BLK_1', 'BLK_2', 'BLK_3', 'BLK_4'],
                     'yield_rate' : cut_yield,
-                    'prod_rate' : {'BLK_1':506, 'BLK_2':506, 'BLK_3':400, 'BLK_4':400}
+                    'prod_ratio' : {'BLK_1':506, 'BLK_2':506, 'BLK_3':400, 'BLK_4':400}
                    }
         # =========================
         
-        # Preparing `stock` dataframe
+        # Preparing `stock` and `delta` dataframes
         self.stock = pd.read_csv(root_dir + "data/stock.csv")  # Hourly
         hours = pd.read_csv(root_dir + "data/sample_submission.csv", usecols=['time'])
         hours.rename(columns = {'time':'hour'}, inplace=True)
         self.stock = self.stock.reindex(hours.index)
         self.stock.insert(0, 'hour', hours['hour'])
+        
+        self.delta = self.stock.copy(deep=True)
+        self.delta.loc[0, self.delta.columns.difference(['hour'], sort=False)] = None
+        self.delta.fillna(0, inplace=True)
         
         # Preprocessing `order` dataframe
         self.order = pd.read_csv(root_dir + "data/order.csv")  # Daily
@@ -49,14 +54,7 @@ class Simulator:
         self.order = self.order.loc[valid_order_idxs]
         self.order['time'] = self.order['time'] + " 18:00:00"
         self.order.rename(columns={'time':'shipping_time'}, inplace=True)
-        
-        # Preparing `max_count` dataframe
-        self.max_count = pd.read_csv(root_dir + "data/max_count.csv")  # Daily
-        self.max_count.set_index('date', inplace=True)
-        
-        # Preparing `change_time` dataframe
-        self.change_time = pd.read_csv(root_dir + "data/change_time.csv")  # Time-independent
-    
+
     def get_score(self, submission):
         """
         Evaluates the given 'submission'
@@ -73,11 +71,12 @@ class Simulator:
         end_idx = schedule.index[-1]
         
         stock = self.stock.copy(deep=True)
+        delta = self.delta.copy(deep=True)
         order = self.order.copy(deep=True)
 
         assert schedule.index.identical(stock.index), "submitted schedule does not have expected number of rows"
         
-        # Records delta PRT on `stock` dataframe
+        # Records delta PRT on `delta` dataframe
         for schedule_idx, row in schedule[self.PRT['types']].iterrows():
             if schedule_idx + self.PRT['elapsed_hours'] > end_idx:
                 break
@@ -86,9 +85,9 @@ class Simulator:
                 if item_input > 0:
                     rate = self.PRT['yield_rate']
                     completed_amount = np.sum(np.random.choice(2, item_input, p=[1-rate, rate]))
-                    stock.loc[schedule_idx + self.PRT['elapsed_hours'], col_label] = completed_amount
+                    delta.loc[schedule_idx + self.PRT['elapsed_hours'], col_label] += completed_amount
         
-        # Records delta MOL on `stock` dataframe
+        # Records delta MOL on `delta` dataframe
         mol_prods = [prod_attr for prod_status in self.MOL['prod_lines'].values() for prod_attr in prod_status]
         line_items = dict.fromkeys(self.MOL['prod_lines'].keys())
         for schedule_idx, row in schedule[mol_prods].iterrows():
@@ -101,32 +100,58 @@ class Simulator:
                 
                 elif (row[f'Event_{line}'] == 'PROCESS') and (row[f'MOL_{line}'] > 0):
                     # Consume the specified amount of corresponding PRT
-                    if stock.loc[schedule_idx, f'PRT_{line_items[line]}'] is None:
-                        stock.loc[schedule_idx, f'PRT_{line_items[line]}'] = 0 - row[f'MOL_{line}']
-                    else:
-                        stock.loc[schedule_idx, f'PRT_{line_items[line]}'] -= row[f'MOL_{line}']
+                    delta.loc[schedule_idx, f'PRT_{line_items[line]}'] -= row[f'MOL_{line}']
                     
                     rate = self.MOL['yield_rate']
                     completed_amount = row[f'MOL_{line}'] * rate
-                    stock.loc[schedule_idx + self.MOL['elapsed_hours'], f'MOL_{line_items[line]}'] = completed_amount
+                    delta.loc[schedule_idx + self.MOL['elapsed_hours'], f'MOL_{line_items[line]}'] += completed_amount
         
-        # Calculate PRT and MOL stocks cumulatively
-        for item_type, item_stock in stock[self.PRT['types'] + self.MOL['types']].iteritems():
-            delta_idxs = item_stock[item_stock.notna()].index
-            for idx_pos in range(1, len(delta_idxs)):
-                curr_idx, prev_idx = delta_idxs[idx_pos], delta_idxs[idx_pos - 1]
-                stock.loc[curr_idx, item_type] += stock.loc[prev_idx, item_type]
-        stock[self.PRT['types'] + self.MOL['types']] = stock[self.PRT['types'] + self.MOL['types']].fillna(method='ffill')
+        # Ship BLKs upon order
+        stock_items = stock.columns.difference(['hour'], sort=False)
+        order_items = order.columns.difference(['shipping_time'], sort=False)
+        blk_diffs = []
+        for stock_idx, stock_info in stock.loc[1:].iterrows():
+            stock.loc[stock_idx, stock_items] = stock.loc[stock_idx - 1, stock_items] + delta.loc[stock_idx, stock_items]
+            query_order = order['shipping_time'].str.contains(stock_info['hour'])
+            if any(query_order):
+                order_amounts = order[query_order == True].iloc[0].loc[order_items]
+                for blk_type, order_amount in order_amounts.iteritems():
+                    if order_amount > 0:
+                        mol_input = stock.loc[stock_idx, f'MOL_{blk_type[-1]}']
+                        ratio = self.BLK['prod_ratio'][blk_type]
+                        rate = self.BLK['yield_rate'].loc[stock_info['hour'][:7], blk_type]
 
+                        # Consume corresponding MOL
+                        stock.loc[stock_idx, f'MOL_{blk_type[-1]}'] = 0
+
+                        # Adding new BLK to BLK stock and subtracting ordered amount
+                        blk_gen = int(mol_input * ratio * rate)
+                        stock.loc[stock_idx, blk_type] += blk_gen
+                        stock.loc[stock_idx, blk_type] -= order_amount
+
+                        # Recording the remaining BLK stock to `blk_diffs`
+                        blk_diffs.append(stock.loc[stock_idx, blk_type])
+
+        # Calculating the score
         """
         To do:
-            1. BLKs should be generated when orders are made.
+            1. Implement the evaluation metrdic used by the leaderboard
         """
+        def f_xa(x, a):
+            return 1 - (x/a) if x < a else 0
+ 
+        score = sum(map(abs, blk_diffs))
+
         # return score
-        return schedule, stock
+        return schedule, delta, stock, blk_diffs, score
 
 if __name__ == "__main__":
     sim = Simulator()
     sample = pd.read_csv(root_dir + "Dacon_baseline.csv")
     
-    schedule, stock = sim.get_score(sample)
+    schedule, delta, stock, blk_diffs, score = sim.get_score(sample)
+    
+    from module.simulator import Simulator as Old_Simulator
+    old_sim = Old_Simulator()
+    
+    old_score, old_out, old_blk_diffs = old_sim.get_score(sample)
