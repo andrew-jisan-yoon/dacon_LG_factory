8722cab481cdd09885fd22a0db776249906276d0
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-08-09 19:48:15 +0900

    created new script with expectations for refactored simulator

---
+++ module/simulator_refactor.py
@@ -0,0 +1,6 @@
+"""
+Simulator class specifications:
+    1. instance attributes stay same no matter how many times get_score is called
+    2. get_score instance function accepts submission csv file
+    3. get_score instance function returns a float value and a dataframe, respectively for a score and a remaining stock
+"""
